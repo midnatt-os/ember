618829879a9184277e62171fff3d08da27733837
vincent <vingus-3@student.ltu.se>
2025-04-20 01:23:10 +0200

    move tooling and configs into the support dir

--- kernel/boot/entry.c
+++ kernel/boot/entry.c
@@ -1,6 +1,6 @@
 #include "limine.h"
 
 int kentry(void) {
-    int i = 1 + 3;
+    int dawdaw = 0;
     return 0;
 }
--- support/generate_img.sh
+++ support/generate_img.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf iso_root
+rm -rf limine
 
 git clone https://github.com/limine-bootloader/limine.git --branch=v9.x-binary --depth=1
 
@@ -9,7 +10,9 @@ make -C limine
 mkdir -p iso_root/EFI/BOOT
 cp limine/BOOTX64.EFI iso_root/EFI/BOOT/
 
-cp limine.conf iso_root/  # copy limine.conf
+rm -rf limine
+
+cp support/limine.conf iso_root/  # copy limine.conf
 cp $1 iso_root/  # copy kernel elf
 
-mkimg --config=mkimg_aloe.toml
+mkimg --config=support/mkimg_aloe.toml
