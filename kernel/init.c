ab7d52f87c2564a673431788e6b3002ae9242235
vincent <vingus-3@student.ltu.se>
2025-04-22 01:51:05 +0200

    init cleanup

--- kernel/init.c
+++ kernel/init.c
@@ -7,22 +7,6 @@
 
 
 
-
-
-#define ACCESS_PRESENT (1 << 7)
-#define ACCESS_DPL(DPL) (((DPL) & 0b11) << 5)
-#define ACCESS_TYPE_TSS (9)
-#define ACCESS_TYPE_CODE(CONFORM, READ) ((1 << 4) | (1 << 3) | ((CONFORM) << 2) | ((READ) << 1))
-#define ACCESS_TYPE_DATA(DIRECTION, WRITE) ((1 << 4) | ((DIRECTION) << 2) | ((WRITE) << 1))
-#define ACCESS_ACCESSED (1 << 0)
-
-#define FLAG_GRANULARITY (1 << 7)
-#define FLAG_DB (1 << 6)
-#define FLAG_LONG (1 << 5)
-#define FLAG_SYSTEM_AVL (1 << 4)
-
-
-
 uintptr_t g_hhdm_offset;
 
 [[noreturn]] void kinit(BootInfo* boot_info) {
