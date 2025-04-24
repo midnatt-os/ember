3bcc5e100a5ed064e8ab4d3fc5217cc02ae2eabb
vincent <vingus-3@student.ltu.se>
2025-04-24 21:03:07 +0200

    generic intrusive doubly linked list implementation

---
+++ include/lib/list.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stddef.h>
+
+#define LIST_NEW (List) { .head = nullptr, .tail = nullptr}
+
+#define LIST_NEXT(NODE) ((NODE)->next)
+#define LIST_PREV(NODE) ((NODE)->prev)
+
+#define LIST_ELEMENT(NODE, TYPE, MEMBER) ((TYPE*) ((uintptr_t) (NODE) - __builtin_offsetof(TYPE, MEMBER)))
+#define LIST_FOREACH(LIST, NODE) for(ListNode* (NODE) = (LIST).head; (NODE) != nullptr; (NODE) = LIST_NEXT(NODE))
+
+typedef struct ListNode {
+    struct ListNode* next;
+    struct ListNode* prev;
+} ListNode;
+
+typedef struct {
+    ListNode* head;
+    ListNode* tail;
+    size_t count;
+} List;
+
+bool list_is_empty(List* list);
+
+void list_node_append(List* list, ListNode* position, ListNode* node);
+void list_node_prepend(List* list, ListNode* position, ListNode* node);
+
+void list_append(List* list, ListNode* node);
+void list_prepend(List* list, ListNode* node);
+void list_delete(List* list, ListNode* node);
---
+++ kernel/lib/list.c
@@ -0,0 +1,85 @@
+#include "lib/list.h"
+
+#include "common/assert.h"
+
+bool list_is_empty(List* list) { return list->count == 0; }
+
+void list_node_append(List* list, ListNode* position, ListNode* node) {
+    ASSERT(position != nullptr);
+    ASSERT(node != nullptr);
+
+    list->count++;
+
+    node->prev = position;
+    node->next = position->next;
+
+    position->next = node;
+
+    if (position == list->tail) {
+        list->tail = node;
+        return;
+    }
+
+    node->next->prev = node;
+}
+
+void list_node_prepend(List* list, ListNode* position, ListNode* node) {
+    ASSERT(position != nullptr);
+    ASSERT(node != nullptr);
+
+    list->count++;
+
+    node->next = position;
+    node->prev = position->prev;
+
+    position->prev = node;
+
+    if (position == list->head) {
+        list->head = node;
+        return;
+    }
+
+    node->prev->next = node;
+}
+
+void list_append(List* list, ListNode* node) {
+    if (list_is_empty(list)) {
+        list->head = node;
+        list->tail = node;
+        list->count++;
+
+        return;
+    }
+
+    list_node_append(list, list->tail, node);
+}
+
+void list_prepend(List* list, ListNode* node) {
+    if (list_is_empty(list)) {
+        list->head = node;
+        list->tail = node;
+        list->count++;
+
+        return;
+    }
+
+    list_node_prepend(list, list->head, node);
+}
+
+void list_delete(List* list, ListNode* node) {
+    ASSERT(node != nullptr);
+
+    if (node == list->head)
+        list->head = node->next;
+
+    if (node == list->tail)
+        list->tail = node->prev;
+
+    if (node->prev != nullptr)
+        node->prev->next = node->next;
+
+    if (node->next != nullptr)
+        node->next->prev = node->prev;
+
+    list->count--;
+}
